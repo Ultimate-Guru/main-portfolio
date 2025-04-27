b50a02340511321c87b54eb3668646a9a5d21be5
Ultimate Guru <tosindaniel360@gmail.com>
2025-04-27 18:40:12 +0100

    refactor About and Hero components: update image imports and adjust project count

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,6 @@
 // Copyright (c) 2024 Oluwatosin
 
 import { MdArrowDownward, MdDownload } from "react-icons/md";
-import HeroImg from "../assets/images/avatar.png";
 import HeroBanner from '../assets/images/image__2.png'
 import { ButtonPrimary, ButtonOutline } from "./Button";
 
@@ -14,22 +13,14 @@ const Hero = () => {
 
                 <div>
                     <div className="flex items-center gap-3">
-                        <figure className="img-box w-9 h-9 rounded-lg">
-                            <img
-                                src={HeroImg}
-                                width={40}
-                                height={40}
-                                alt="Portrait"
-                                className="img-cover"
-                            />
-                        </figure>
+                        
 
-                        <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
+                        {/* <div className="flex items-center gap-1.5 text-zinc-400 text-sm tracking-wide">
                             <span className="relative w-2 h-2 rounded-full bg-emerald-400">
                                 <span className="absolute inset-0 rounded-full bg-emerald-400 animate-ping"></span>
                             </span>
                             Available for work
-                        </div>
+                        </div> */}
                     </div>
 
                     <h2 className="headLine-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
@@ -38,7 +29,7 @@ const Hero = () => {
 
                     <div className="flex items-center gap-5">
                         <ButtonPrimary
-                            href='mainresume.pdf'
+                            href='My-Resume.pdf'
                             download
                             label="Download CV"
                             icon={<MdDownload />}
