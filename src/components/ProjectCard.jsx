4f83a2279ba3d7afa8c6733b43daba9ff7622940
Oluwatosin <tosindaniel360@gmail.com>
2026-02-05 20:16:22 +0100

    fix: adjust ring opacity in ProjectCard component

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -13,7 +13,7 @@ const ProjectCard = ({
     classes
 }) => {
     return (
-        <div className={"relative p-4 rounded-2xl bg-zinc-800 hover:bg-zinc-700/50 active:bg-zinc-700/60 ring-1 ring-inset ring-zinc-50/50 transition-colors " + classes}>
+        <div className={"relative p-4 rounded-2xl bg-zinc-800 hover:bg-zinc-700/50 active:bg-zinc-700/60 ring-1 ring-inset ring-zinc-50/25 transition-colors " + classes}>
 
             <figure className="img-box aspect-square rounded-lg mb-4">
                 <img
