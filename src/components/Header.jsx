ef7b0abe8b15f5f9be950068e982ac58dd12dd19
Oluwatosin <tosindaniel360@gmail.com>
2026-03-24 12:39:24 +0100

    fix: comment out unused Logo import and header element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@
 import { useState } from "react";
 import { MdClose, MdMenu } from "react-icons/md";
 import Navabar from './Navbar'
-import Logo from '../assets/images/tosin_builds-removed.png'
+// import Logo from '../assets/images/tosin_builds-removed.png'
 
 const Header = () => {
 
@@ -15,7 +15,7 @@ const Header = () => {
         <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
             <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]">
 
-                <h1>
+                {/* <h1>
                     <a href="/" className="logo font-bold text-2xl md:text-3xl text-shadow">
                         <img
                             src={Logo}
@@ -23,7 +23,7 @@ const Header = () => {
                             className="md:w-32 lg:w-36 w-24"
                         />
                     </a>
-                </h1>
+                </h1> */}
 
                 <div className="relative md:justify-self-center">
                     <button
